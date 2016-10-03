27c6215d3304ef2afdad95d9c80ef171af94a1ae
Raghu Betina <raghu@firstdraft.com>
2016-10-03 17:17:26 +0000

    Direct association between companies and users

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,9 @@
 class Company < ApplicationRecord
   # Direct associations
 
+  has_many   :users,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Direct associations
 
+  belongs_to :company
+
   has_many   :comments,
              :foreign_key => "commenter_id",
              :dependent => :destroy
